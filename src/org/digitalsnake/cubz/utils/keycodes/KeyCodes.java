57ebdadc4d2636dd3d97e38b78856c092f9d3912
ImTallone <39484479+CristeaAndreiFlavian@users.noreply.github.com>
2018-09-06 09:52:15 +0300

    More KeyCodes

--- src/org/digitalsnake/cubz/utils/keycodes/KeyCodes.java
+++ src/org/digitalsnake/cubz/utils/keycodes/KeyCodes.java
@@ -1,15 +1,72 @@
 package org.digitalsnake.cubz.utils.keycodes;
 
-import static org.lwjgl.glfw.GLFW.*;
+import org.lwjgl.glfw.GLFW;
 
 public class KeyCodes {
 
-	public static final int KEY_W = GLFW_KEY_W;
-	public static final int KEY_S = GLFW_KEY_S;
-	public static final int KEY_A = GLFW_KEY_A;
-	public static final int KEY_D = GLFW_KEY_D;
-	public static final int KEY_SPACE = GLFW_KEY_SPACE;
-	public static final int KEY_SHIFT1 = GLFW_KEY_LEFT_SHIFT;
-	public static final int KEY_SHIFT2 = GLFW_KEY_RIGHT_SHIFT;
+	// Letters
+	public static final int KEY_Q = GLFW.GLFW_KEY_Q;
+	public static final int KEY_W = GLFW.GLFW_KEY_W;
+	public static final int KEY_E = GLFW.GLFW_KEY_E;
+	public static final int KEY_R = GLFW.GLFW_KEY_R;
+	public static final int KEY_T = GLFW.GLFW_KEY_T;
+	public static final int KEY_Y = GLFW.GLFW_KEY_Y;
+	public static final int KEY_U = GLFW.GLFW_KEY_U;
+	public static final int KEY_I = GLFW.GLFW_KEY_I;
+	public static final int KEY_O = GLFW.GLFW_KEY_O;
+	public static final int KEY_P = GLFW.GLFW_KEY_P;
+	public static final int KEY_A = GLFW.GLFW_KEY_A;
+	public static final int KEY_S = GLFW.GLFW_KEY_S;
+	public static final int KEY_D = GLFW.GLFW_KEY_D;
+	public static final int KEY_F = GLFW.GLFW_KEY_F;
+	public static final int KEY_G = GLFW.GLFW_KEY_G;
+	public static final int KEY_H = GLFW.GLFW_KEY_H;
+	public static final int KEY_J = GLFW.GLFW_KEY_J;
+	public static final int KEY_K = GLFW.GLFW_KEY_K;
+	public static final int KEY_L = GLFW.GLFW_KEY_L;
+	public static final int KEY_Z = GLFW.GLFW_KEY_Z;
+	public static final int KEY_X = GLFW.GLFW_KEY_X;
+	public static final int KEY_C = GLFW.GLFW_KEY_C;
+	public static final int KEY_V = GLFW.GLFW_KEY_V;
+	public static final int KEY_B = GLFW.GLFW_KEY_B;
+	public static final int KEY_N = GLFW.GLFW_KEY_N;
+	public static final int KEY_M = GLFW.GLFW_KEY_M;
+	
+	// Numbers
+	public static final int KEY_0 = GLFW.GLFW_KEY_0;
+	public static final int KEY_1 = GLFW.GLFW_KEY_1;
+	public static final int KEY_2 = GLFW.GLFW_KEY_2;
+	public static final int KEY_3 = GLFW.GLFW_KEY_3;
+	public static final int KEY_4 = GLFW.GLFW_KEY_4;
+	public static final int KEY_5 = GLFW.GLFW_KEY_5;
+	public static final int KEY_6 = GLFW.GLFW_KEY_6;
+	public static final int KEY_7 = GLFW.GLFW_KEY_7;
+	public static final int KEY_8 = GLFW.GLFW_KEY_8;
+	public static final int KEY_9 = GLFW.GLFW_KEY_9;
+	
+	// Arrows
+	public static final int KEY_UP = GLFW.GLFW_KEY_UP;
+	public static final int KEY_DOWN = GLFW.GLFW_KEY_DOWN;
+	public static final int KEY_RIGHT = GLFW.GLFW_KEY_RIGHT;
+	public static final int KEY_LEFT = GLFW.GLFW_KEY_LEFT;
+	
+	// "F" Characters
+	public static final int KEY_F1 = GLFW.GLFW_KEY_F1;
+	public static final int KEY_F2 = GLFW.GLFW_KEY_F2;
+	public static final int KEY_F3 = GLFW.GLFW_KEY_F3;
+	public static final int KEY_F4 = GLFW.GLFW_KEY_F4;
+	public static final int KEY_F5 = GLFW.GLFW_KEY_F5;
+	public static final int KEY_F6 = GLFW.GLFW_KEY_F6;
+	public static final int KEY_F7 = GLFW.GLFW_KEY_F7;
+	public static final int KEY_F8 = GLFW.GLFW_KEY_F8;
+	public static final int KEY_F9 = GLFW.GLFW_KEY_F9;
+	
+	// Special Characters
+	public static final int KEY_CTRL1 = GLFW.GLFW_KEY_LEFT_CONTROL;
+	public static final int KEY_CTRL2 = GLFW.GLFW_KEY_RIGHT_CONTROL;
+	public static final int KEY_SHIFT1 = GLFW.GLFW_KEY_LEFT_SHIFT;
+	public static final int KEY_SHIFT2 = GLFW.GLFW_KEY_RIGHT_SHIFT;
+	public static final int KEY_ESCAPE = GLFW.GLFW_KEY_ESCAPE;
+	public static final int KEY_SPACE = GLFW.GLFW_KEY_SPACE;
 	
 }
